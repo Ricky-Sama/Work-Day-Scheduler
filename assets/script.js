1148bb4b7fdc62d750013990305297490cd22e81
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-07-30 15:07:05 -0400

    removed comments and added functions for saving/loading to/from local storage

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,62 @@
-  // ````// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
-
 // Display current date function
+$(function () {
+  displayCurrentDate();
+});
+
 function displayCurrentDate() {
   const currentDayElement = $('#currentDay');
   const currentDate = dayjs().format('dddd: MMMM D, YYYY');
   currentDayElement.text(`${currentDate}`);
 }
 
+//COLORS OF ROWS
 $(function () {
-  displayCurrentDate();
+  setRowColorBasedOnTime();
 });
 
-
-
-// //COLORS OF ROWS
-// function setRowColorBasedOnTime() {
-//   const currentHour = new Date().getHours();
+function setRowColorBasedOnTime() {
+  const currentHour = new Date().getHours();
 
   
-//   $('.time-block').each(function () {
-//       const rowHour = parseInt($(this).attr('id').split('-')[1]);
+  $('.time-block').each(function () {
+      const rowHour = parseInt($(this).attr('id').split('-')[1]);
 
-//       if (rowHour < currentHour) {
-//           $(this).addClass('past').removeClass('present future');
-//       } else if (rowHour === currentHour) {
-//           $(this).addClass('present').removeClass('past future');
-//       } else {
-//           $(this).addClass('future').removeClass('past present');
-//       }
-//   });
-// }
+      if (rowHour < currentHour) {
+          $(this).addClass('past').removeClass('present future');
+      } else if (rowHour === currentHour) {
+          $(this).addClass('present').removeClass('past future');
+      } else {
+          $(this).addClass('future').removeClass('past present');
+      }
+  });
+}
 
+// Function to save event description to local storage
+function saveEventToLocalStorage(eventId, eventDescription) {
+  localStorage.setItem(eventId, eventDescription);
+}
 
-// $(function () {
-//   setRowColorBasedOnTime();
-// });
+// Function to load events from local storage (called when the page loads)
+function loadEventsFromLocalStorage() {
+  $('.time-block').each(function () {
+      const eventId = $(this).attr('id');
+      const eventDescription = localStorage.getItem(eventId);
+      if (eventDescription) {
+          $(this).find('textarea').val(eventDescription);
+      }
+  });
+}
 
+// Call the displayCurrentDate and loadEventsFromLocalStorage functions when the DOM is ready
+$(function () {
+  displayCurrentDate();
+  loadEventsFromLocalStorage();
 
+  // Save Function
+  $('.saveBtn').on('click', function () {
+      const eventRow = $(this).closest('.time-block');
+      const eventId = eventRow.attr('id');
+      const eventDescription = eventRow.find('textarea').val();
+      saveEventToLocalStorage(eventId, eventDescription);
+  });
+});
